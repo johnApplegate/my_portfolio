f8010d6eab601871cb5c28042eeb4688631eafb1
johnApplegate <applegation@gmail.com>
2024-01-31 16:59:59 +0000

    finally added all the laptop links and imags, sized them for mobile only

--- pfl_script.js
+++ pfl_script.js
@@ -1,4 +1,4 @@
-// script.js
+
 document.addEventListener('DOMContentLoaded', function () {
     var greyTitles = document.querySelectorAll('.greyTitle');
 
@@ -10,4 +10,32 @@ document.addEventListener('DOMContentLoaded', function () {
 
     setTimeout(addSlideInClass, 1000);
 });
+
+
+
+const boxes = document.querySelectorAll('.project_content');
+
+window.addEventListener('scroll', checkBoxes); 
+
+checkBoxes();
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
+
 
